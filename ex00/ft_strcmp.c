699f37c1a287dbc2d9ef83e6dd7996628da2797b
Alberto Zaragoza <alberzh@gmail.com>
2025-07-28 12:45:25 +0200

    Ejercicio ex00 Listo.

---
+++ ex00/ft_strcmp.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 11:11:42 by azaragoz          #+#    #+#             */
+/*   Updated: 2025/07/28 12:43:46 by azaragoz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] && str1[i] == str2[i])
+	{
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}
+
+/* #include <stdio.h>
+
+int	main(void)
+{
+	char *str1 = "Hello";
+	char *str2 = "Hello";
+	char *str3 = "World";
+	char *str4 = "Hell";
+	char *str5 = "";
+	char *str6 = "";
+
+	printf("Comparando '%s' y '%s': %i\n", str1, str2, ft_strcmp(str1, str2));
+	printf("Comparando '%s' y '%s': %i\n", str1, str3, ft_strcmp(str1, str3));
+	printf("Comparando '%s' y '%s': %i\n", str3, str1, ft_strcmp(str3, str1));
+	printf("Comparando '%s' y '%s': %i\n", str1, str4, ft_strcmp(str1, str4));
+	printf("Comparando '%s' y '%s': %i\n", str4, str1, ft_strcmp(str4, str1));
+	printf("Comparando '%s' y '%s': %i\n", str5, str6, ft_strcmp(str5, str6));
+	printf("Comparando '%s' y '%s': %i\n", str1, str5, ft_strcmp(str1, str5));
+
+	return (0);
+} */
