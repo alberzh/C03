4d0ce36e162d39e24199b43444b28be4c155e67d
Alberto Zaragoza <alberzh@gmail.com>
2025-07-29 11:32:12 +0200

    Ejercicio ex04 Listo.

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,30 +6,50 @@
 /*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 17:11:12 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/28 17:23:39 by azaragoz         ###   ########.fr       */
+/*   Updated: 2025/07/29 11:26:09 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = 0;
-    j = 0;
-    if (to_find[0])
-	    return (str);
+	i = 0;
+	j = 0;
+	if (!to_find[0])
+		return (str);
 	while (str[i])
 	{
 		while (str[i + j] == to_find[j])
 		{
-			if (to_find[j + i])
+			if (!to_find[j + 1])
 			{
 				return (&str[i]);
 			}
 			j++;
 		}
+		j = 0;
 		i++;
 	}
 	return (0);
 }
+
+/* #include <stdio.h>
+
+int	main(void)
+{
+	char str[50] = "Hola, ¿Qué tal? ¿Cómo estás?";
+	char to_find[12] = " tal";
+
+	char *result = ft_strstr(str, to_find);
+	if (result)
+	{
+		printf("Subcadena encontrada: '%s'\n", result);
+	}
+	else
+	{
+		printf("Subcadena no encontrada.\n");
+	}
+	return (0);
+} */
